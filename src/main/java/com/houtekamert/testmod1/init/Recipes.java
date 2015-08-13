3f13ed8e0302030bfab057cf812278c40478a4fb
houtekamert <tomhoutekamer@gmail.com>
2015-08-13 13:41:49 +0200

    Testmod1
    fixed crashing error caused by broken recipes

--- src/main/java/com/houtekamert/testmod1/init/Recipes.java
+++ src/main/java/com/houtekamert/testmod1/init/Recipes.java
@@ -1,5 +1,6 @@
 package com.houtekamert.testmod1.init;
 
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -14,21 +15,19 @@ public class Recipes
         GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf));
 
         //tools
-        GameRegistry.addRecipe(new ItemStack(ModItems.qbsword), " s ", " s ", "t", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.ebstick));
-        GameRegistry.addRecipe(new ItemStack(ModItems.qbpick), "sss", " t ", " t ", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.ebstick));
-        GameRegistry.addRecipe(new ItemStack(ModItems.qbaxe), "ss ", "st ", "t", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.ebstick));
-        GameRegistry.addRecipe(new ItemStack(ModItems.qbshovel), " s ", " t ", "t", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.ebstick));
-        GameRegistry.addRecipe(new ItemStack(ModItems.qbhoe), "ss ", " t ", "t", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.ebstick));
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.qbsword), " s ", " s ", " e ", 's', new ItemStack(ModItems.qbsteel), 'e', new ItemStack(ModItems.ebstick));
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.qbaxe), "ss ", "se ", " e ", 's', new ItemStack(ModItems.qbsteel), 'e', new ItemStack(ModItems.ebstick));
+        
 
         //blocks
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.qcore), "dsd", "sts", "dsd", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.corefragment), 'd', new ItemStack(Items.diamond) );
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.qcore), "dsd", "sts", "dsd", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(ModItems.corefragment), 'd', new ItemStack(Items.diamond));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.qcable), "sss", "ttt", "sss", 's', new ItemStack(ModItems.qbsteel), 't', new ItemStack(Items.string));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.qconnector), " s ", "sts", " s ", 's', new ItemStack(ModBlocks.qcable), 't', new ItemStack(ModItems.corefragment));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.qtop), "sss", " t ", "   ", 's', new ItemStack(Items.iron_ingot), 't', new ItemStack(ModBlocks.qcable));
 
 
         //items
-        GameRegistry.addRecipe(new ItemStack(ModItems.ebstick), "  ", " s ", " s ", 's', new ItemStack(Items.ender_pearl));
+        GameRegistry.addRecipe(new ItemStack(ModItems.ebstick), "   ", " s ", " s ", 's', new ItemStack(Items.ender_pearl));
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.qbsteel), new ItemStack(ModItems.biodust,2), new ItemStack(Items.iron_ingot), new ItemStack(Items.coal));
 
         //smelting
